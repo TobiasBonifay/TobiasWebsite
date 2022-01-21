7e7457f8817e04ed1e1b90036547be4b81021198
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-01-21 13:19:05 +0100

    svg Draw NOT A RELEASE

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// execute script after page load
 window.onload = function digital_fn(){
 
     // toggle button
@@ -30,47 +29,70 @@ var controller = new ScrollMagic.Controller();
 
 new ScrollMagic.Scene({triggerElement: "#digitalLifeTrigger1"})
     .setClassToggle("#grid-images1", "revealBottom1") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 new ScrollMagic.Scene({triggerElement: "#digitalLifeTrigger1"})
     .setClassToggle("#grid-images2", "revealBottom2") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 new ScrollMagic.Scene({triggerElement: "#digitalLifeTrigger1"})
     .setClassToggle("#grid-images3", "revealBottom3") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
 new ScrollMagic.Scene({triggerElement: "#digitalLifeTrigger2"})
     .setClassToggle("#futurSoft, #imCurrently", "revealLeft") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 new ScrollMagic.Scene({triggerElement: "#digitalLifeTrigger2"})
     .setClassToggle("#bornNear, #moreAbout", "revealRight") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
 new ScrollMagic.Scene({triggerElement: "#countingAreaTrigger"})
     .setClassToggle("#theHundred", "revealLeft") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 new ScrollMagic.Scene({triggerElement: "#countingAreaTrigger"})
     .setClassToggle("#ifYouAreCurrently", "revealRight") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 new ScrollMagic.Scene({triggerElement: "#countingAreaTrigger"})
     .setClassToggle("#grid-two", "revealBottom") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
 new ScrollMagic.Scene({triggerElement: "#recentWorkTrigger"})
     .setClassToggle("#flexItem1, #flexItem2, #flexItem3", "revealTop") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
 new ScrollMagic.Scene({triggerElement: "#articleAreaTrigger"})
     .setClassToggle("#card1, #card2, #card3", "revealTop") // add class toggle
-    .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
+
+/** SVG **/
+function pathPrepare ($el) {
+    var lineLength = $el[0].getTotalLength();
+    $el.css("stroke-dasharray", lineLength);
+    $el.css("stroke-dashoffset", lineLength);
+}
+
+
+var $word = $("#word");
+var $dot = $("#dot");
+/* var $word = document.getElementById("word");
+var $dot = document.getElementById("dot") */
+
+// prepare SVG
+pathPrepare($word);
+pathPrepare($dot);
+
+// init controller
+// const controller = new ScrollMagic.Controller();
+
+// build tween
+/* var tween = gsap.TimelineMax() */
+var tween = new TimelineMax()
+    .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease: Linear.easeNone})) // draw word for 0.9
+    .add(TweenMax.to($dot, 0.1, {strokeDashoffset: 0, ease: Linear.easeNone}))  // draw dot for 0.1
+    .add(TweenMax.to("path", 1, {stroke: "#33629c", ease: Linear.easeNone}), 0);			// change color during the whole thing
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 200, tweenChanges: true})
+    .setTween(tween)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
 
